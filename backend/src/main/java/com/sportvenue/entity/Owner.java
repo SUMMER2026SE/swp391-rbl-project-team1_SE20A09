81c5847d31ca4ca038bd8f4a8f055baf18c7c1cf
Leon <fanadn2019@gmail.com>
2026-06-01 02:33:25 +0700

    chore(checkstyle): remove unused imports in Owner and Stadium entities

--- backend/src/main/java/com/sportvenue/entity/Owner.java
+++ backend/src/main/java/com/sportvenue/entity/Owner.java
@@ -3,8 +3,6 @@ package com.sportvenue.entity;
 import com.sportvenue.entity.enums.ApprovedStatus;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
--- backend/src/main/java/com/sportvenue/entity/Stadium.java
+++ backend/src/main/java/com/sportvenue/entity/Stadium.java
@@ -4,8 +4,6 @@ import com.sportvenue.entity.enums.StadiumStatus;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
