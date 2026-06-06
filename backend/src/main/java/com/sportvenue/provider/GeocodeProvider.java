b564ebd7a8bf0ca0ad996259d73a8de0031004f0
Xuanhuy218972 <xuanhuy21082023@gmail.com>
2026-06-06 21:47:09 +0700

    feat(be-infra): integrate Goong Geocoding API and geocoding endpoints

---
+++ backend/src/main/java/com/sportvenue/provider/GeocodeProvider.java
@@ -0,0 +1,9 @@
+package com.sportvenue.provider;
+
+import com.sportvenue.dto.response.LocationDTO;
+import java.util.List;
+
+public interface GeocodeProvider {
+    List<LocationDTO> search(String query);
+    LocationDTO reverseGeocode(Double lat, Double lng);
+}
